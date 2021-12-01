73068ebf26ea2ad3724d32aeb05778848908b8a1
Reja <rezanurrochmat@gmail.com>
2021-12-01 15:24:39 +0700

    Add mailer

---
+++ src/main/java/com/mailer/demo/MailerController.java
@@ -0,0 +1,34 @@
+package com.mailer.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+@RestController
+@RequestMapping("/api")
+public class MailerController {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    @PostMapping("/send")
+    public ResponseEntity<String> postSendEmail(@RequestParam String email) throws AddressException {
+        SimpleMailMessage msg = new SimpleMailMessage();
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+        msg.setFrom("no-reply-reja@gmail.com");
+        msg.setTo(email);
+
+        javaMailSender.send(msg);
+
+        return new ResponseEntity<String>("Sended email", HttpStatus.CREATED);
+
+    }
+}
